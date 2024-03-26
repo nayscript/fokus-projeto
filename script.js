949b785e865e3671d72e2636bc5e7beb6ad66578
Nayrlen Alves <161717698+nayscript@users.noreply.github.com>
2024-03-26 11:21:13 -0300

    feat: funcionalidade som adicionada.

--- script.js
+++ script.js
@@ -5,6 +5,29 @@ const longoBtn = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
+const startPauseBtn = document.querySelector('#start-pause');
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const iniciarOupausarBtn = document.querySelector('#start-pause span');
+const iniciarOupausarIcone = document.getElementsByClassName('app__card-primary-butto-icon');
+
+const musica = new Audio('sons/luna-rise-part-one.mp3');
+const play = new Audio('sons/play.wav');
+const pause = new Audio('sons/pause.mp3');
+const beep = new Audio('sons/beep.mp3');
+
+
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
+
+musica.loop = true;
+
+musicaFocoInput.addEventListener('change', () => {
+    if (musica.paused) {
+        musica.play();
+    } else {
+        musica.pause();
+    }
+})
 
 focoBtn.addEventListener('click', () => {
     alterarContexto('foco');
@@ -52,4 +75,35 @@ function alterarContexto(contexto) {
         default:
             break;
     }
+}
+
+const contagemRegressiva = () => {
+    if (tempoDecorridoEmSegundos <= 0) {
+        // beep.play();
+        zerar();
+        alert('Tempo finalizado!');
+        return;
+    }
+    tempoDecorridoEmSegundos -= 1;
+    console.log(`Temporizador: ${tempoDecorridoEmSegundos}`);
+}
+
+startPauseBtn.addEventListener('click', iniciarOupausar);
+
+function iniciarOupausar() {
+    play.play();
+    if (intervaloId) {
+        pause.play();
+        zerar();
+        return;
+    }
+    intervaloId = setInterval(contagemRegressiva, 1000);
+    iniciarOupausarBtn.textContent = "Pausar";
+    iniciarOupausarIcone.setAttribute = ('src', 'imagens/pause.png');
+}
+
+function zerar() {
+    clearInterval(intervaloId);
+    iniciarOupausarBtn.textContent = "Começar";
+    intervaloId = null;
 }
